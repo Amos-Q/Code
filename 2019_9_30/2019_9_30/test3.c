f32594e768441550263555a81ac23cd5d980c39f
Amos <1334819112@qq.com>
2019-09-30 22:52:14 +0800

    上传

---
+++ 2019_9_30/2019_9_30/test3.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	int n = 0;
+//	for (i = 0; i <= 13; i+=2)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//		n = i-2;
+//	}
+//	for (i = n; i >= 0; i-=2)
+//	{
+//		for (j = i; j >= 0; j--)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	for (i = 100; i <= 999; i++)
+//	{
+//		a = i / 100;
+//		b = (i - a * 100) / 10;
+//		c = i - a * 100 - b * 10;
+//		if ( pow(a,3) + pow(b,3) + pow(c,3) == i )
+//			printf("%d ", i);
+//	}
+//	system("pause");
+////	return 0;
+////}
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	scanf("%d", &i);
+//	sum = i + (i + i * 10) + (i + i * 10 + i * 100) + (i + i * 10 + i * 100 + i * 1000) + (i + i * 10 + i * 100 + i * 1000 + i * 10000);
+//	printf("sum = %d\n", sum);
+//	system("pause");
+//	return 0;
+//}
