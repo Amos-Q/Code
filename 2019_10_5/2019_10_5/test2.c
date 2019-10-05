2d6fe2727b50b61b1e279a183ed7729463f5bac9
Amos <1334819112@qq.com>
2019-10-05 23:22:40 +0800

    上传

--- 2019_10_5/2019_10_5/test2.c
+++ 2019_10_5/2019_10_5/test2.c
@@ -2,22 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Js(int n, int k)
+void Js(int n, int k)
 {
-	if (k > 1)
-		return n * Js(n, k - 1);
-	else
-		return n;
+	int i = 0;
+	int sum = n;
+	for (i = 1; i < k; i++)
+		sum *= n;
+	printf("%d\n", sum);
 }
 int main()
 {
 	int n = 0;
 	int k = 0;
-	int sum = 0;
 	printf("Input the number:");
 	scanf("%d %d", &n, &k);
-	sum = Js(n, k);
-	printf("%d\n", sum);
+	Js(n, k);
 	system("pause");
 	return 0;
 }
