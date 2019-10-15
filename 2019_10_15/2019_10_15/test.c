f18315bb048879ff86446c643b919d1a74af0364
Amos <1334819112@qq.com>
2019-10-15 09:27:03 +0800

    上传

---
+++ 2019_10_15/2019_10_15/test.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int n, i;
+	printf("ÇëÊäÈëÕûÊı£º");
+	scanf("%d", &n);
+	printf("%d=", n);
+	for (i = 2; i <= n; i++)
+	{
+		while (n%i == 0)
+		{
+			printf("%d", i);
+			n /= i;
+			if (n != 1) printf("*");
+		}
+	}
+
+	printf("\n");
+	system("pause");
+	return 0;
+}
