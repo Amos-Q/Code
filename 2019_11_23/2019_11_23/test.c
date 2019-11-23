41b1ff454096fad528fe1a89641879d390954647
Amos <1334819112@qq.com>
2019-11-23 09:39:39 +0800

    上传

--- 2019_11_23/2019_11_23/test.c
+++ 2019_11_23/2019_11_23/test.c
@@ -3,60 +3,60 @@
 #include <windows.h>
 #include <assert.h>
 
-char *my_strncpy(char *dst, const char *src, int n)
-{
-	assert(dst);
-	assert(src);
-	assert(n > 0);
-	char *cpy = dst;
-	while (n--)
-	{
-		*dst++ = *src++;
-	}
-	return cpy;
-
-}
-
-int main()
-{
-	char a[20] = "abcdefgh";
-	char *p = "1234";
-	int n = 0;
-	char *ret = my_strncpy(a, p, 5);
-	printf("ret: %s\n", ret);
-
-	system("pause");
-	return 0;
-}
-char *my_strncat(char *des, const char *src, int count)
-{
-	assert(des);
-	assert(src);
-	char *cat = des;
-	int i = 0;
-	while (*des != '\0')
-	{
-		des++;
-	}
-	for (; i < count; i++)
-	{
-		*des = *src;
-		des++;
-		*src++;
-	}
-	return cat;
-}
-
-int main()
-{
-	char a[10] = { 'a', 'b', 'c', '\0' };
-	char b[20] = "world and bit";
-	int n = 5;
-	char *ret = my_strncat(a, b, n);
-	printf("ret: %s\n", ret);
-	system("pause");
-	return 0;
-}
+//char *my_strncpy(char *dst, const char *src, int n)
+//{
+//	assert(dst);
+//	assert(src);
+//	assert(n > 0);
+//	char *cpy = dst;
+//	while (n--)
+//	{
+//		*dst++ = *src++;
+//	}
+//	return cpy;
+//
+//}
+//
+//int main()
+//{
+//	char a[20] = "abcdefgh";
+//	char *p = "1234";
+//	int n = 0;
+//	char *ret = my_strncpy(a, p, 5);
+//	printf("ret: %s\n", ret);
+//
+//	system("pause");
+//	return 0;
+//}
+//char *my_strncat(char *des, const char *src, int count)
+//{
+//	assert(des);
+//	assert(src);
+//	char *cat = des;
+//	int i = 0;
+//	while (*des != '\0')
+//	{
+//		des++;
+//	}
+//	for (; i < count; i++)
+//	{
+//		*des = *src;
+//		des++;
+//		*src++;
+//	}
+//	return cat;
+//}
+//
+//int main()
+//{
+//	char a[10] = { 'a', 'b', 'c', '\0' };
+//	char b[20] = "world and bit";
+//	int n = 5;
+//	char *ret = my_strncat(a, b, n);
+//	printf("ret: %s\n", ret);
+//	system("pause");
+//	return 0;
+//}
 
 
 int my_strncmp(const char *str1, const char *str2, int n)
