2128c76e864713986c4ac271d7c3c36557b567fa
Amos <1334819112@qq.com>
2019-12-05 23:29:32 +0800

    上传

---
+++ SeqList/SeqList/seqlist.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"home.h"
+
+
+void SeqListInit(SeqList* psl, size_t capacity)
+{
+	psl->array = NULL;
+
+}
+void SeqListDestory(SeqList* psl)
+{
+
+}
+void SeqListPrint(SeqList* psl)
+void CheckCapacity(SeqList* psl)
+void SeqListPushBack(SeqList* psl, SLDataType x)
+void SeqListPopBack(SeqList* psl)
+void SeqListPushFront(SeqList* psl, SLDataType x)
+void SeqListPopFront(SeqList* psl)
+int SeqListFind(SeqList* psl, SLDataType x)
+void SeqListInsert(SeqList* psl, size_t pos, SLDataType x)
+void SeqListErase(SeqList* psl, size_t pos)
