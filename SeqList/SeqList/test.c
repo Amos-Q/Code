79d585add08d7e94f526dc08256d7fa18dd28284
Amos <1334819112@qq.com>
2019-12-05 23:29:43 +0800

    上传

---
+++ SeqList/SeqList/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "home.h"
+
+int main()
+{
+	SeqListInit(psl, capacity)
+	SeqListDestory(SeqList* psl)
+	SeqListPrint(SeqList* psl)
+	CheckCapacity(SeqList* psl)
+	SeqListPushBack(SeqList* psl, SLDataType x)
+	SeqListPopBack(SeqList* psl)
+	SeqListPushFront(SeqList* psl, SLDataType x)
+	SeqListPopFront(SeqList* psl)
+	SeqListFind(SeqList* psl, SLDataType x)
+	SeqListInsert(SeqList* psl, size_t pos, SLDataType x)
+	SeqListErase(SeqList* psl, size_t pos)
+	system("pause");
+	return 0;
+}
