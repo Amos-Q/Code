4044abf36175e126b7a11f5c2cf78b01211c7407
Amos <1334819112@qq.com>
2019-12-26 10:54:50 +0800

    上传

---
+++ 银行家算法/银行家算法/home.h
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int bool;
+#define false 0
+#define true !false
+
+bool SafeCheck(int Max[10][10], int Allocation[10][10], int Need[10][10], int Available[10], int safe[10], int MAX_PROCESS, int MAX_RESOURCE);
+void ProbeAlloc(int Allocation[10][10], int Need[10][10], int Available[10], int process, int res[10], int MAX_RESOURCE);
+void RollBack(int Allocation[10][10], int Need[10][10], int Available[10], int process, int res[10], int MAX_PROCESS, int MAX_RESOURCE);
+void PrintTable(int Max[10][10], int Allocation[10][10], int Need[10][10], int Available[10],int MAX_PROCESS, int MAX_RESOURCE);
+bool request(int Max[10][10], int process, int res[10], int Allocation[10][10], int Need[10][10], int Available[10], int safe[10], int MAX_PROCESS, int MAX_RESOURCE);
