1916c3423aa2765f9f9b29939fb18080701086f6
Amos <1334819112@qq.com>
2019-12-30 09:56:08 +0800

    修改

--- 银行家算法/银行家算法/home.h
+++ 银行家算法/银行家算法/home.h
@@ -5,7 +5,6 @@
 
 typedef int bool;
 #define false 0
-#define true !false
 
 bool SafeCheck(int Max[10][10], int Allocation[10][10], int Need[10][10], int Available[10], int safe[10], int MAX_PROCESS, int MAX_RESOURCE);
 void ProbeAlloc(int Allocation[10][10], int Need[10][10], int Available[10], int process, int res[10], int MAX_RESOURCE);
