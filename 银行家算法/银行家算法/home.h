ff2a6dee07b2563022ed89e5f4b9964a0a6c493d
Amos <1334819112@qq.com>
2020-01-06 10:10:28 +0800

    修改

--- 银行家算法/银行家算法/home.h
+++ 银行家算法/银行家算法/home.h
@@ -2,10 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 typedef int bool;
 #define false 0
-
 bool SafeCheck(int Max[10][10], int Allocation[10][10], int Need[10][10], int Available[10], int safe[10], int MAX_PROCESS, int MAX_RESOURCE);
 void ProbeAlloc(int Allocation[10][10], int Need[10][10], int Available[10], int process, int res[10], int MAX_RESOURCE);
 void RollBack(int Allocation[10][10], int Need[10][10], int Available[10], int process, int res[10], int MAX_PROCESS, int MAX_RESOURCE);
