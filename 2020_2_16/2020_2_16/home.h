a210bf15ef3f9ae01e6e17e8d33c597fde5827ee
Amos <1334819112@qq.com>
2020-02-16 19:37:53 +0800

    上传

---
+++ 2020_2_16/2020_2_16/home.h
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+struct student
+{
+	int num;
+	char name[20];
+	float pingshi;
+	float shiyan;
+	float kaoshi;
+	double zongping;
+}stu[4];
+
+void a();
+void b();
+void c();
+void d();
+void e();
