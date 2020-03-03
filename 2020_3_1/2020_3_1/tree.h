313ce5ed60c20aa5626bdd780f7bf0ebb18348b6
Amos <1334819112@qq.com>
2020-03-03 12:16:54 +0800

    修改

--- 2020_3_1/2020_3_1/tree.h
+++ 2020_3_1/2020_3_1/tree.h
@@ -2,6 +2,8 @@
 #pragma once
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <assert.h>
 
 typedef int HPDataType;
 typedef struct Heap
@@ -12,11 +14,15 @@ typedef struct Heap
 }Heap;
 
 // 堆的构建
-void HeapCreate(Heap* hp, HPDataType* a, int n);
+Heap* HeapCreate(HPDataType* a, int n);
+//向下调整
 void AdjustDown(int* a, int root, int n);
-
+//向上调整算法
+void AdjustUp(int* a, int root, int n);
 // 堆的销毁
 void HeapDestory(Heap* hp);
+//交换函数
+void Swap(HPDataType* left, HPDataType *right);
 // 堆的插入
 void HeapPush(Heap* hp, HPDataType x);
 // 堆的删除
@@ -27,14 +33,11 @@ HPDataType HeapTop(Heap* hp);
 int HeapSize(Heap* hp);
 // 堆的判空
 int HeapEmpty(Heap* hp);
-
 // 对数组进行堆排序
 void HeapSort(int* a, int n);
-
 // TopK问题：找出N个数里面最大/最小的前K个问题。
 // 比如：未央区排名前10的泡馍，西安交通大学王者荣耀排名前10的韩信，全国排名前10的李白。等等问题都是Topk问题，
 // 需要注意：
 // 找最大的前K个，建立K个数的小堆
 // 找最小的前K个，建立K个数的大堆
 void PrintTopK(int* a, int n, int k);
-void TestTopk();
