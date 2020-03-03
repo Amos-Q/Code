d3c6e5a445ab0265cb90de391ca835f690bba3e6
Amos <1334819112@qq.com>
2020-03-03 12:15:05 +0800

    修改

--- 2020_3_1/2020_3_1/test.c
+++ 2020_3_1/2020_3_1/test.c
@@ -5,6 +5,10 @@
 int main()
 {
 
+	int arr[10] = { 23, 56, 78, 9, 3, 4, 66, 88, 34, 25 };
+	/*HeapCreate(&hp, arr, 10);
+	HeapSort(arr, 10);*/
+	PrintTopK(arr, 10, 3);
 	system("pause");
 	return 0;
 }
