26e9d717cd0355dd18aac983f4739bd31fb736a7
Amos <1334819112@qq.com>
2020-03-18 10:42:42 +0800

    修改

--- 2020_3_12/2020_3_12/Btree.h
+++ 2020_3_12/2020_3_12/Btree.h
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
 
 typedef char BTDataType;
+
 typedef struct BinaryTreeNode
 {
 	BTDataType _data;
@@ -13,10 +15,15 @@ typedef struct BinaryTreeNode
 
 // 通过前序遍历的数组"ABD##E#H##CF##G##"构建二叉树
 BTNode* BinaryTreeCreate(BTDataType* a, int n, int* pi);
+//销毁
 void BinaryTreeDestory(BTNode* root);
+//节点数
 int BinaryTreeSize(BTNode* root);
+//叶子节点数
 int BinaryTreeLeafSize(BTNode* root);
+//第K层数
 int BinaryTreeLevelKSize(BTNode* root, int k);
+//查询
 BTNode* BinaryTreeFind(BTNode* root, BTDataType x);
 
 // 遍历
@@ -26,5 +33,4 @@ void BinaryTreePostOrder(BTNode* root);
 
 // 层序遍历
 void BinaryTreeLevelOrder(BTNode* root);
-
 void testTree();
