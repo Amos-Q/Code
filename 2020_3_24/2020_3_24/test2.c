effd9905c22ea53f1a39721d42afa6d41587caec
Amos <1334819112@qq.com>
2020-03-25 15:33:16 +0800

    修改

--- 2020_3_24/2020_3_24/test2.c
+++ 2020_3_24/2020_3_24/test2.c
@@ -64,3 +64,67 @@ public:
 
 		delete root;
 	}
+//创建一棵二叉树
+	node* _create_tree(T*a, size_t n, const T& invalid, size_t& index)
+	{
+		node* root = NULL;
+		if (a[index] != invalid)
+		{
+			root = new node(a[index]);
+			root->_left = _create_tree(a, n, invalid, ++index);
+			root->_right = _create_tree(a, n, invalid, ++index);
+		}
+		return root;
+	}
+
+	//前序遍历
+	void prev_order()
+	{
+		_prev_order(_root);
+		cout << endl;
+	}
+	void _prev_order(node*root)
+	{
+		if (root == NULL)
+			return;
+		cout << root->_data << "  ";
+		_prev_order(root->_left);
+		_prev_order(root->_right);
+	}
+
+	//非递归的前序遍历
+	void prev_order_no_r()
+	{
+		node* cur = _root;
+		stack<node*>s;
+		while (cur || !s.empty())
+		{
+			while (cur)
+			{
+				cout << cur->_data << "  ";
+				s.push(cur);
+				cur = cur->_left;
+			}
+			node* top = s.top();
+			s.pop();
+			//子问题
+			cur = top->_right;
+		}
+		cout << endl;
+	}
+
+	//中序遍历
+	void in_order()
+	{
+		_in_order(_root);
+		cout << endl;
+	}
+	void _in_order(node* root)
+	{
+		//中序遍历：左子树->根节点->右子树
+		if (root == NULL)return;
+
+		_in_order(root->_left);
+		cout << root->_data << "  ";
+		_in_order(root->_right);
+	}
