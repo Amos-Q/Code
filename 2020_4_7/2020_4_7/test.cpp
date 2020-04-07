d8641aebda5b68816176a7b35dbbb04a4c8b92cc
Amos <1334819112@qq.com>
2020-04-07 15:51:39 +0800

    上传

---
+++ 2020_4_7/2020_4_7/test.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+using namespace std;
+
+//#define add( left,  right){ cout << left+right  <<  endl;}
+
+//inline int add(int left, int right)
+//{
+//	return left + right;
+//}
+//int main()
+//{
+//	int ret = add(1, 2);
+//	//add(1,2);
+//	system("pause");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//int a = 0;
+//	//auto b = a;
+//	//cout << typeid(a).name() << endl;
+//	int arr[] = { 1,2,3,4 };
+//	for (auto& e : arr)
+//		cout << 2 * e << " ";
+//	cout << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//void fun(int){}
+//void fun(int*){}
+//
+//int main()
+//{
+//	int* pl = NULL;
+//	int* p2 = nullptr;
+//	fun(NULL);
+//	fun(nullptr);
+//}
+
+//struct person
+//class person
+//{
+//public:
+//	void showinfo() 
+//	{
+//		cout << _name << endl;
+//	}
+//	int getage()
+//	{
+//		return _age;
+//	}
+////private:
+//	char* _name;
+//	int _age;
+//	char _sex;
+//	//....
+//};
+//
+//int main()
+//{
+//	person p1;
+//	p1._name = "peter";
+//	p1._age = 18;
+//
+//}
+
