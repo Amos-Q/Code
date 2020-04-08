562ee27a08ca88c610a8c4e18b604dc33083ac56
Amos <1334819112@qq.com>
2020-04-08 19:11:55 +0800

    上传

---
+++ 2020_4_8/2020_4_8/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	char arr[20];
+//	scanf("%s", &arr);
+//	for (int x = 0; x < strlen(arr); x++)
+//	{
+//		for (int y = x; y < strlen(arr); y++)
+//		{
+//			if (arr[y] < arr[x])
+//			{
+//				char tmp = 0;
+//				tmp = arr[x];
+//				arr[x] = arr[y];
+//				arr[y] = tmp;
+//			}
+//		}
+//	}
+//	printf("%s", arr);
+//	return 0;
+//}
