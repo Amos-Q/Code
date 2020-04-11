6367bbcdb5599dabb4f5fde24a14745bf6b092aa
Amos <1334819112@qq.com>
2020-04-11 20:33:29 +0800

    上传

--- 2020_4_10/2020_4_10/test.cpp
+++ 2020_4_10/2020_4_10/test.cpp
@@ -34,3 +34,40 @@ void MagicShow(const Point& p, bool flag = false, bool info = false)  //ÔÚµãp´¦Ê
 	if (info || g[p.x][p.y] == space)
 		PrintAt(p, flag ? square : space);
 }
+void Block::State_Init()
+{
+	//I
+	all_state[0][0] = all_state[0][2] = "0100 0100 0100 0100";
+	all_state[0][1] = all_state[0][3] = "0000 1111 0000 0000";
+	//O
+	all_state[1][0] = all_state[1][1] = "0000 0110 0110 0000";
+	all_state[1][2] = all_state[1][3] = "0000 0110 0110 0000";
+	//S
+	all_state[2][0] = all_state[2][2] = "0000 0110 1100 0000";
+	all_state[2][1] = all_state[2][3] = "0100 0110 0010 0000";
+	//Z
+	all_state[3][0] = all_state[3][2] = "0000 1100 0110 0000";
+	all_state[3][1] = all_state[3][3] = "0100 1100 1000 0000";
+	//T
+	all_state[4][0] = "0000 1110 0100 0000";
+	all_state[4][1] = "0100 1100 0100 0000";
+	all_state[4][2] = "0100 1110 0000 0000";
+	all_state[4][3] = "0100 0110 0100 0000";
+	//J
+	all_state[5][0] = "0100 0100 1100 0000";
+	all_state[5][1] = "1000 1110 0000 0000";
+	all_state[5][2] = "1100 1000 1000 0000";
+	all_state[5][3] = "0000 1110 0010 0000";
+	//L
+	all_state[6][0] = "0100 0100 0110 0000";
+	all_state[6][1] = "0000 1110 1000 0000";
+	all_state[6][2] = "1100 0100 0100 0000";
+	all_state[6][3] = "0010 1110 0000 0000";
+}
+Block::Block()
+{
+	id = rand() % 7;                      //Ëæ»úÉú³É¿éµÄÖÖÀà
+	dir = rand() % 4;                     //Ëæ»úÉú³É¿éµÄ·½Ïò
+	p = Point(4, 13);                    //³õÊ¼»¯µãpµÄÎ»ÖÃ
+	StateCpy();
+}
