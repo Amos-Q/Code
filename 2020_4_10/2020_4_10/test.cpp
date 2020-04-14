c4d4b2a5ff0e23dd2de8b6051424ed123fe2e696
Amos <1334819112@qq.com>
2020-04-14 13:57:51 +0800

    上传

--- 2020_4_10/2020_4_10/test.cpp
+++ 2020_4_10/2020_4_10/test.cpp
@@ -90,3 +90,71 @@ void Block::Show(bool info = false)           //info表示是否是信息栏,下同
 		for (int j = 0; j < 4; j++)
 			MagicShow(Point(p.x + i, p.y + j), state[i][j], info);
 }
+Game::Game() :level(1), score(0)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			g[i][j] = "  ";
+		for (int j = 0; j < MM; j++)
+			gg[i][j] = "  ";
+	}
+	for (int i = 1; i < M; i++)
+		g[0][i] = g[N - 1][i] = line[0];
+	for (int i = 1; i < MM; i++)
+		gg[0][i] = gg[N - 1][i] = line[0];
+	for (int i = 1; i < N; i++)
+		g[i][0] = g[i][M - 1] = gg[i][0] = gg[i][MM - 1] = line[1];
+	g[0][0] = gg[0][0] = corner[0];
+	g[0][M - 1] = gg[0][MM - 1] = corner[1];
+	g[N - 1][0] = gg[N - 1][0] = corner[2];
+	g[N - 1][M - 1] = gg[N - 1][MM - 1] = corner[3];
+}
+
+int Game::GetTickCnt()
+{
+	return tick_cnt[level];
+}
+
+void Game::ShowFrame()
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+			cout << g[i][j];
+		for (int j = 0; j < MM; j++)
+			cout << gg[i][j];
+		cout << endl;
+	}
+	PrintAt(Point(2, 13), " Next");
+	PrintAt(Point(9, 13), " Level");
+	PrintAt(Point(10, 14), level, 2);
+	PrintAt(Point(12, 13), " Score");
+	PrintAt(Point(13, 14), score, 2);
+	PrintAt(Point(20, 13), " @Xiaod");
+}
+
+bool Game::AllSquare(int xx)
+{
+	for (int j = 1; j < M - 1; j++)
+		if (g[xx][j] != square)
+			return false;
+	return true;
+}
+
+void Game::DropDown(int xx)
+{
+	for (int i = xx; i > 1; i--)
+		for (int j = 1; j < M - 1; j++)
+			g[i][j] = g[i - 1][j];
+	for (int j = 1; j < M - 1; j++)
+		g[1][j] = space;
+}
+
+void Game::Update(int flash_line_cnt)
+{
+	score += score_up[flash_line_cnt];
+	PrintAt(Point(13, 14), score);
+	if (level < 6 && score >= level_up[level])
+		PrintAt(Point(10, 14), ++level, 2);
+}
