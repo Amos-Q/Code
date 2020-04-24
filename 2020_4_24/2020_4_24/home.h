8e2c751b59d742f0f836b1461bfa7555a8065d4f
Amos <1334819112@qq.com>
2020-04-24 20:48:17 +0800

    上传

---
+++ 2020_4_24/2020_4_24/home.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include <set>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <functional>
+using namespace std;
+char arr[3][3];
