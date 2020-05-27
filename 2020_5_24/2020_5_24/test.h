20e6cb0b27f3c5ce67ad425c58e64cf8de2dae36
Amos <1334819112@qq.com>
2020-05-27 20:51:49 +0800

    上传

--- 2020_5_24/2020_5_24/test.h
+++ 2020_5_24/2020_5_24/test.h
@@ -40,4 +40,46 @@ private:
 
 
 };
+//初始化扑克牌，发牌位置，庄家与各玩家手中的扑克牌数
+Point21::Point21()
+{
+	int nowPos = 0;	//当前扑克牌位置
 
+	char* str;
+
+	for (int shapePos = 0; shapePos < 4; shapePos++)	//花色
+	{
+		for (int numPos = 1; numPos <= 13; numPos++)	//面值
+		{
+			PK[nowPos].shape = (shapetype)shapePos;	//花色
+			PK[nowPos].num = (numtype)numPos;	//面值
+			nowPos++;
+		}
+	}
+	cout << endl << "请问有几个人参加游戏？(最多7个人):";
+	str = new char[1];
+	cin >> str;
+	numgamer = atoi(str);
+	//atoi函数与itoa()函数区别 
+
+	while (numgamer < 1 || numgamer>7) //限制人数在1~7之间
+	{
+		cout << endl << "玩家人数限定在1~7个,请重新输入人数：";
+		cin >> str;
+		numgamer = atoi(str);
+	}
+
+	startPosition = 0;	//发牌位置
+	int i = 0;	//临时变量
+	//庄家numcards[0]及玩家numcards[1~7]手中的扑克牌张数
+	for (i = 0; i <= numgamer; i++) numcards[i] = 0;
+
+	strcpy(name[0], "庄家");
+
+	for (i = 1; i <= numgamer; i++)   //玩家姓名
+	{
+		cout << "输入第" << i << "个玩家的姓名:";
+		cin >> name[i];
+	}
+	cout << "游戏开始" << endl << endl;
+}
