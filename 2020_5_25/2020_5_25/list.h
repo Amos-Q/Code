4577cfd59fe339278984af89c69c335f2f99d2c7
Amos <1334819112@qq.com>
2020-05-28 23:28:12 +0800

    修改

--- 2020_5_25/2020_5_25/list.h
+++ 2020_5_25/2020_5_25/list.h
@@ -16,12 +16,13 @@ namespace fxd
 		{}
 	};
 
+	// __list_iterator<T, T&, T*>   -> iterator
+	// __list_iterator<T, const T&, const T*>   -> const_iterator
 	template<class T, class Ref, class Ptr>
-	//template<class T>
-	//template<class T, class T&, class T*>
 	struct __list_iterator
 	{
 		typedef __list_node<T> Node;
+		typedef __list_iterator<T, Ref, Ptr> Self;
 		Node* _node;
 
 		__list_iterator(Node* node)
@@ -34,37 +35,38 @@ namespace fxd
 			return _node->_data;
 		}
 
+		// it->
 		Ptr operator->()
 		{
 			return &_node->_data;
 		}
 
 		// ++it;
-		__list_iterator<T>& operator++()
+		Self& operator++()
 		{
 			_node = _node->_next;
 			return *this;
 		}
 
 		// it++
-		__list_iterator<T> operator++(int)
+		Self operator++(int)
 		{
-			__list_iterator<T> tmp(*this);
+			Self tmp(*this);
 			//_node = _node->_next;
 			++(*this);
 
 			return tmp;
 		}
 
-		__list_iterator<T>& operator--()
+		Self& operator--()
 		{
 			_node = _node->_prev;
 			return *this;
 		}
 
-		__list_iterator<T> operator--(int)
+		Self operator--(int)
 		{
-			__list_iterator<T> tmp(*this);
+			Self tmp(*this);
 			//_node = _node->_prev;
 			--(*this);
 
@@ -72,34 +74,42 @@ namespace fxd
 		}
 
 		// it != end()
-		bool operator!=(const __list_iterator<T>& it)
+		bool operator!=(const Self& it)
 		{
 			return _node != it._node;
 		}
 
-		bool operator==(const __list_iterator<T>& it)
+		bool operator==(const Self& it)
 		{
 			return _node == it._node;
 		}
 	};
 
-	template<class T>
-	class list
-	{
-		typedef __list_node<T> Node;
-	public:
-		 typedef __list_iterator<T> iterator;
-		// 思考const_iterator是如何实现，提示：可以去分析一下原代码
-		//typedef __list_iterator<T, const T&, const T*> const_iterator;
-		//typedef __list_iterator<T> iterator;
-		iterator begin()
+		template<class T>
+		class list
 		{
+			typedef __list_node<T> Node;
+		public:
+			typedef __list_iterator<T, T&, T*> iterator;
+			// 思考const_iterator是如何实现，提示：可以去分析一下原代码
+			typedef __list_iterator<T, const T&, const T*> const_iterator;
+
+		iterator begin(){
 			return iterator(_head->_next);
 		}
 
 		iterator end()
 		{
 			return iterator(_head);
+		}
+
+		const_iterator begin() const{
+			return const_iterator(_head->_next);
+		}
+
+		const_iterator end() const
+		{
+			return const_iterator(_head);
 		}
 
 		// 带头双向循环链表
@@ -110,20 +120,46 @@ namespace fxd
 			_head->_prev = _head;
 		}
 
+		// lt2(lt1)
 		list(const list<T>& lt)
 		{
 			_head = new Node;
 			_head->_next = _head;
 			_head->_prev = _head;
-			const_iterator it = lt.begin();
-			while (it != lt.end())
-			{
-				this->pop_back(*it);
-				push_back(*it);
-				++it;
-			}
+
+			//const_iterator it = lt.begin();
+			//while (it != lt.end())
+			//{
+			//	//this->pop_back(*it);
+			//	push_back(*it);
+			//	++it;
+			//}
+
+			for (auto e : lt)
+				push_back(e);
 		}
-		//list<T>& operator=(const list<T>& lt);
+
+		// lt1 = lt3
+		// lt1 = lt1
+	/*	list<T>& operator=(const list<T>& lt)
+		{
+			if (this != &lt)
+			{
+				clear();
+				for (auto e : lt)
+					push_back(e);
+			}
+
+			return *this;
+		}*/
+
+		// lt1 = lt3
+		list<T>& operator=(list<T> lt)
+		{
+			swap(_head, lt._head);
+			return *this;
+		}
+
 		~list()
 		{
 			clear();
@@ -142,23 +178,27 @@ namespace fxd
 
 		void push_back(const T& x)
 		{
-			Node* tail = _head->_prev;
+			/*Node* tail = _head->_prev;
 			Node* newnode = new Node(x);
 			
 			tail->_next = newnode;
 			newnode->_prev = tail;
 			newnode->_next = _head;
-			_head->_prev = newnode;
+			_head->_prev = newnode;*/
+			insert(end(), x);
 		}
 
 		void pop_back()
 		{
+			//erase(iterator(_head->_prev));
 			erase(--end());
 		}
+
 		void push_front(const T& x)
 		{
 			insert(begin(), x);
 		}
+
 		void pop_front()
 		{
 			erase(begin());
@@ -169,19 +209,27 @@ namespace fxd
 			Node* cur = pos._node;
 			Node* prev = cur->_prev;
 			Node* newnode = new Node(x);
+
+			// prev newnode cur
 			prev->_next = newnode;
 			newnode->_prev = prev;
 			newnode->_next = cur;
 			cur->_prev = newnode;
 		}
+
 		void erase(iterator pos)
 		{
 			assert(pos != end());
+
 			Node* cur = pos._node;
-			cur->_prev->_next = cur->_next;
-			cur->_next->_prev = cur->_prev;
+			Node* prev = cur->_prev;
+			Node* next = cur->_next;
 			delete cur;
+
+			prev->_next = next;
+			next->_prev = prev;
 		}
+
 	private:
 		Node* _head;
 	};
@@ -220,17 +268,65 @@ namespace fxd
 
 		list<Date> lt;
 		lt.push_back(Date());
-		//lt.push_back(Date());
+		lt.push_back(Date());
 
 		list<Date>::iterator it = lt.begin();
 		while (it != lt.end())
 		{
 			//cout << *it << " ";
-			cout << it->_day << "-" << it->_month << "-" << it->_day << endl; // 更喜欢这么用
-			cout << (*it)._day << "-" << (*it)._month << "-" << (*it)._day << endl;
+			cout << it->_year << "-" << it->_month << "-" << it->_day << endl; // 更喜欢这么用
+			cout << (*it)._year << "-" << (*it)._month << "-" << (*it)._day << endl;
 
 			++it;
 		}
 		cout << endl;
+	}
+
+	void print_list(const list<int>& lt)
+	{
+		list<int>::const_iterator it = lt.begin();
+		while (it != lt.end())
+		{
+			//*it = 1;
+			cout << *it << " ";
+			++it;
+		}
+		cout << endl;
+	}
+
+	void test_list3()
+	{
+		list<int> lt;
+		lt.push_back(1);
+		lt.push_back(2);
+		lt.push_back(3);
+		lt.push_back(4);
+
+		print_list(lt);
+
+		lt.pop_back();
+		lt.pop_front();
+		print_list(lt);
+
+		lt.clear();
+		lt.push_back(10);
+	}
+
+	void test_list4()
+	{
+		list<int> lt1;
+		lt1.push_back(1);
+		lt1.push_back(2);
+
+		list<int> lt2(lt1);
+		print_list(lt2);
+
+		list<int> lt3;
+		lt3.push_back(10);
+		lt3.push_back(20);
+		lt3.push_back(30);
+		lt3.push_back(40);
+		lt1 = lt3;
+		print_list(lt1);
 	}
 }
